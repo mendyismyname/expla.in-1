f35b1363f865cde9b2dc6e85ef32c85260be653c
GeorgeTaveras1231 <george.taveras1231@gmail.com>
2014-07-05 13:07:21 -0400

    updated notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,8 +4,8 @@ class Notification < ActiveRecord::Base
   belongs_to :sender, class: User
   belongs_to :reciever, class: User
 
-  scope :recent, -> {
-    order( :created_at => :desc )
+  scope :recent, lambda{ |limit = 15|
+    order( :created_at => :desc ).limit( limit )
   }
 
 end
