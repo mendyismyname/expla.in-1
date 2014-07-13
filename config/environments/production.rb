af7aa0f5850873fb6abc40424ecc8ec948a847c8
Mendy <seldo6@gmail.com>
2014-07-12 22:09:22 -0400

    undoing some stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,6 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = 
 
-  config.font_assets.origin = 'http://tasbir-li.herokuapp.com/'
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
 
