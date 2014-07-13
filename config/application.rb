93b82751dc75a24cec4ae2fa51ca09bd8097b718
Mendy <seldo6@gmail.com>
2014-07-12 22:02:19 -0400

    app crashed

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module ExplaIn
     
     config.middleware.delete Rack::Lock
     config.middleware.use(FayeRails::Middleware, mount: '/explained') do
-
+    
+    config.assets.paths << Rails.root.join('lib', 'assets', 'fonts')
+    config.assets.precompile += %w(.svg .eot .woff .ttf)
     
       map '/notifications/**' => NotificationsController
       
